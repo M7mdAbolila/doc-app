13d6b557144c055c444349b777a1b7c4e3e68267
M7mdAbolila <m7md.abolila.cs1@gmail.com>
2024-03-21 13:32:07 +0200

    - design system - app router - app colors

--- lib/doc_app.dart
+++ lib/doc_app.dart
@@ -1,10 +1,26 @@
+import 'package:docdoc/core/routing/app_router.dart';
+import 'package:docdoc/core/routing/routes.dart';
+import 'package:docdoc/core/theme/colors.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class DocApp extends StatelessWidget {
-  const DocApp({super.key});
-
+  const DocApp({super.key, required this.appRouter});
+  final AppRouter appRouter;
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return  ScreenUtilInit(
+      designSize: const Size(375, 812),
+      minTextAdapt: true,
+      child: MaterialApp(
+        title: 'Doc App',
+        theme: ThemeData(
+          primaryColor: DocColors.mainBlue,
+        ),
+        onGenerateRoute: appRouter.generateRoute,
+        initialRoute: Routes.onBoardingScreen,
+        home:const Scaffold(),
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
+import 'package:docdoc/core/routing/app_router.dart';
 import 'package:docdoc/doc_app.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const DocApp());
+  runApp( DocApp(appRouter: AppRouter(),));
 }
 
